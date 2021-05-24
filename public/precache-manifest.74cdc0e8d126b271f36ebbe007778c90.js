74a8fd8ab9a003a6ff57f93fe697dfa969c106ad
eeefg650 <rodadin96@gmail.com>
2021-05-24 18:32:38 +0300

    ff

---
+++ public/precache-manifest.74cdc0e8d126b271f36ebbe007778c90.js
@@ -0,0 +1,182 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a19016e4735fc91d546a26d52df5be31",
+    "url": "/ReactifolioBs4_Akj/index.html"
+  },
+  {
+    "revision": "a3696b517362694357f0",
+    "url": "/ReactifolioBs4_Akj/static/css/main.1e220c3f.chunk.css"
+  },
+  {
+    "revision": "47566f2885446ec772eb",
+    "url": "/ReactifolioBs4_Akj/static/js/2.ac1b8024.chunk.js"
+  },
+  {
+    "revision": "cf1b129b5c272ab5c924fa9150c8b22d",
+    "url": "/ReactifolioBs4_Akj/static/js/2.ac1b8024.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3696b517362694357f0",
+    "url": "/ReactifolioBs4_Akj/static/js/main.1027aedd.chunk.js"
+  },
+  {
+    "revision": "cc343884a3b9b4e25004",
+    "url": "/ReactifolioBs4_Akj/static/js/runtime-main.1c1e4ce9.js"
+  },
+  {
+    "revision": "9730fefc26bb187c4f4bb29b95d82e7a",
+    "url": "/ReactifolioBs4_Akj/static/media/Robofriends.9730fefc.webp"
+  },
+  {
+    "revision": "ddc80b55231d534350379f2a651dfb6a",
+    "url": "/ReactifolioBs4_Akj/static/media/Translate-Project.ddc80b55.png"
+  },
+  {
+    "revision": "21999f09aca6272cd15a16d949434923",
+    "url": "/ReactifolioBs4_Akj/static/media/accenture-3.21999f09.svg"
+  },
+  {
+    "revision": "bf989778565c49da91f9e0ef13dbdc5c",
+    "url": "/ReactifolioBs4_Akj/static/media/background.bf989778.webp"
+  },
+  {
+    "revision": "e95d05630894e5403e569fdaa0a2b61a",
+    "url": "/ReactifolioBs4_Akj/static/media/background.e95d0563.webp"
+  },
+  {
+    "revision": "ffded2381d25dd906406d6e07f91dea5",
+    "url": "/ReactifolioBs4_Akj/static/media/bootstrap-4.ffded238.svg"
+  },
+  {
+    "revision": "db3c445b93fa7336d63f617a5f087c7e",
+    "url": "/ReactifolioBs4_Akj/static/media/brain.db3c445b.webp"
+  },
+  {
+    "revision": "3707da3c6a394c7ee2523273f8d98a25",
+    "url": "/ReactifolioBs4_Akj/static/media/css3.3707da3c.svg"
+  },
+  {
+    "revision": "add239172d50d6ad18d2ac3cea6257a1",
+    "url": "/ReactifolioBs4_Akj/static/media/digital-ocean.add23917.svg"
+  },
+  {
+    "revision": "bcf22073c9f13bab1761b90bce9fec55",
+    "url": "/ReactifolioBs4_Akj/static/media/django.bcf22073.svg"
+  },
+  {
+    "revision": "c6bab64b183f96aa47077fd3db37afd0",
+    "url": "/ReactifolioBs4_Akj/static/media/express.c6bab64b.svg"
+  },
+  {
+    "revision": "d98463abd102acdc2b1eb8d5eb70b2d2",
+    "url": "/ReactifolioBs4_Akj/static/media/flask.d98463ab.svg"
+  },
+  {
+    "revision": "0692ddac9614e47c40a43276cd9287e4",
+    "url": "/ReactifolioBs4_Akj/static/media/get_github_info.0692ddac.webp"
+  },
+  {
+    "revision": "1190a19ce3cc940741a60137a1e074cb",
+    "url": "/ReactifolioBs4_Akj/static/media/git-icon.1190a19c.svg"
+  },
+  {
+    "revision": "0555bfb7e81f57a1efe6a53c7490abb7",
+    "url": "/ReactifolioBs4_Akj/static/media/github-api.0555bfb7.svg"
+  },
+  {
+    "revision": "0ff1a6f08182be4aa63a0b54da1b9e57",
+    "url": "/ReactifolioBs4_Akj/static/media/github.0ff1a6f0.svg"
+  },
+  {
+    "revision": "30bd42ea97a2e0454bb1dfbab983eb54",
+    "url": "/ReactifolioBs4_Akj/static/media/go-6.30bd42ea.svg"
+  },
+  {
+    "revision": "b38ff61aa327fbdc24f7b1dad7d04d1a",
+    "url": "/ReactifolioBs4_Akj/static/media/heroku.b38ff61a.svg"
+  },
+  {
+    "revision": "676e8188f003ed5d4882a0d0f378dad3",
+    "url": "/ReactifolioBs4_Akj/static/media/html-5.676e8188.svg"
+  },
+  {
+    "revision": "2d2e31cc63a96c7e6044f52d30fd21b9",
+    "url": "/ReactifolioBs4_Akj/static/media/javascript.2d2e31cc.svg"
+  },
+  {
+    "revision": "f5f9574bc252332ba7942e5d56fbcc28",
+    "url": "/ReactifolioBs4_Akj/static/media/logow.f5f9574b.png"
+  },
+  {
+    "revision": "82291019f34ab54a677c456a84127cc3",
+    "url": "/ReactifolioBs4_Akj/static/media/material-ui-1.82291019.svg"
+  },
+  {
+    "revision": "3ea10fb644c05e7c947611dae8d58a45",
+    "url": "/ReactifolioBs4_Akj/static/media/mern_tip_calc.3ea10fb6.webp"
+  },
+  {
+    "revision": "2985235df31b8742d893d5b7e80145c5",
+    "url": "/ReactifolioBs4_Akj/static/media/mongodb.2985235d.svg"
+  },
+  {
+    "revision": "54d7bddcd3019ec3c3b23db00160ccef",
+    "url": "/ReactifolioBs4_Akj/static/media/mssql.54d7bddc.svg"
+  },
+  {
+    "revision": "a123152868245dee71c441124a98ac3e",
+    "url": "/ReactifolioBs4_Akj/static/media/nodejs.a1231528.svg"
+  },
+  {
+    "revision": "95dd3e00f0920b6bed086d4a30d5b027",
+    "url": "/ReactifolioBs4_Akj/static/media/portfolioUsingDjango.95dd3e00.webp"
+  },
+  {
+    "revision": "d7c985e7cca077dc2d77f89a66573e35",
+    "url": "/ReactifolioBs4_Akj/static/media/postgresql.d7c985e7.svg"
+  },
+  {
+    "revision": "4bef4c3e368bd761b543e597d90b79bc",
+    "url": "/ReactifolioBs4_Akj/static/media/product_hunt_clone.4bef4c3e.webp"
+  },
+  {
+    "revision": "ee3fbd0ef8697ae9b71e3de9cf6856ca",
+    "url": "/ReactifolioBs4_Akj/static/media/profile.ee3fbd0e.jpg"
+  },
+  {
+    "revision": "ef194ddf5faf63cb6cc580a4535be695",
+    "url": "/ReactifolioBs4_Akj/static/media/react-bootstrap.ef194ddf.svg"
+  },
+  {
+    "revision": "024196716802e272adb3cbefb81fd557",
+    "url": "/ReactifolioBs4_Akj/static/media/react-router.02419671.svg"
+  },
+  {
+    "revision": "4009eef3b3e291f296e71962c70895c5",
+    "url": "/ReactifolioBs4_Akj/static/media/react.4009eef3.svg"
+  },
+  {
+    "revision": "aa0a337603611296524a666b62a9ff23",
+    "url": "/ReactifolioBs4_Akj/static/media/redux.aa0a3376.svg"
+  },
+  {
+    "revision": "8bce7aef886c3872c218e0e6ef8bdccb",
+    "url": "/ReactifolioBs4_Akj/static/media/slide1.8bce7aef.webp"
+  },
+  {
+    "revision": "7256f309520276be70acd97720610bc6",
+    "url": "/ReactifolioBs4_Akj/static/media/slide2.7256f309.webp"
+  },
+  {
+    "revision": "8e5b87b60151049a8df9ec0bd30978f0",
+    "url": "/ReactifolioBs4_Akj/static/media/slide3.8e5b87b6.webp"
+  },
+  {
+    "revision": "9ceedb838144da63d409d778f4245a82",
+    "url": "/ReactifolioBs4_Akj/static/media/styled-components.9ceedb83.svg"
+  },
+  {
+    "revision": "3de182d2120823fe49c27eeb1765c53a",
+    "url": "/ReactifolioBs4_Akj/static/media/typescript.3de182d2.svg"
+  }
+]);
