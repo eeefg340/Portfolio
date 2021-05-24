d46b4b1548cd26e42c2e01e2eb5e61031046f093
eeefg650 <rodadin96@gmail.com>
2021-05-24 21:56:35 +0300

    x

--- Config/Keys.js
+++ Config/Keys.js
@@ -1,6 +1,4 @@
 dbPassword =
   "mongodb+srv://eeefg:Osnat1308@cluster0-396al.gcp.mongodb.net/SubtitlesProject?authSource=admin&replicaSet=Cluster0-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true";
 
-module.exports = {
-  mongoURI: dbPassword,
-};
+export default dbPassword
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 // const passport = require("passport");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
-const db = require("./Config/Keys").mongoUdRI;
+const db = require("./Config/Keys").default;
 
 // Connect to MongoDB
 mongoose
