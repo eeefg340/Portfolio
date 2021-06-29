c8b9c530038ef9f936618db2a8f4082e55e225ef
eeefg650 <rodadin96@gmail.com>
2021-06-29 19:52:09 +0300

    Update NodeMailer.js

--- routes/NodeMailer.js
+++ routes/NodeMailer.js
@@ -11,7 +11,8 @@ Router.post("/mail", async (req, res) => {
     await BodyPromise(body);
 
     let transport = nodemailer.createTransport({
-      service: "Gmail",
+      host: "smtp.gmail.com",
+      port: 465,
       secure: true,
 
       auth: {
@@ -21,7 +22,7 @@ Router.post("/mail", async (req, res) => {
     });
 
     let mailOptions = {
-      from: "naor0003@gmail.com",
+      from: `${process.env.USER}`,
       to: "dinroda123@gmail.com",
       subject: "פניה חדשה",
       text: `פנייה חדשה מתוך תיק העבודות : 
