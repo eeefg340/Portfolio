6372410e929c636c6a1e7c2bc26616e447556234
eeefg650 <rodadin96@gmail.com>
2021-06-30 14:07:48 +0300

    Update NodeMailer.js

--- routes/NodeMailer.js
+++ routes/NodeMailer.js
@@ -13,8 +13,8 @@ Router.post("/mail", async (req, res) => {
 
     let transport = nodemailer.createTransport({
       host: "smtp.ethereal.email",
-      port: 587,
-      secure: false, // true for 465, false for other ports
+      port: 465,
+      secure: true, // true for 465, false for other ports
       auth: {
         user: testAccount.user, // generated ethereal user
         pass: testAccount.pass, // generated ethereal password
