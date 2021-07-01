3bdb1adda3992e4623a3c83618f2d19d3fba0379
eeefg650 <rodadin96@gmail.com>
2021-07-01 16:55:36 +0300

    Update Keys.js

--- Config/Keys.js
+++ Config/Keys.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 
-dbPassword = process.env.DBKEY;
+dbPassword =
+  "mongodb+srv://eeefg:Osnat1308@cluster0-396al.gcp.mongodb.net/SubtitlesProject?authSource=admin&replicaSet=Cluster0-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true";
 
-module.exports = dbPassword;
+module.exports = dbPassword
